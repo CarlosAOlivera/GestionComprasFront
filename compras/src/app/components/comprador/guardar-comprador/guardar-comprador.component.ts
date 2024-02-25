584e1dc344a1936d5f685e127aca4288cf40fc36
Carlos A Olivera <91080828+CarlosAOlivera@users.noreply.github.com>
2024-02-25 02:41:50 -0400

    Refactor de validaciones de formulario

--- compras/src/app/components/comprador/guardar-comprador/guardar-comprador.component.ts
+++ compras/src/app/components/comprador/guardar-comprador/guardar-comprador.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { IComprador } from '../../../data/IComprador';
 import { CompradorService } from '../../../data/comprador.service';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -13,7 +13,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./guardar-comprador.component.css']
 })
 export class GuardarCompradorComponent implements OnInit {
-  myForm!: FormGroup;    
+  myForm!: FormGroup;
   submitted = false; 
 
   constructor(
@@ -30,15 +30,26 @@ export class GuardarCompradorComponent implements OnInit {
       IdComprador:['58650f7d-2495-4a2c-9092-493dc2ecda63'], // Temporary Guid
       Nombres: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
       Apellidos: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-      CorreoElectronico: ['', [Validators.required, Validators.email, Validators.maxLength(30)]],
-      TipoDeDocumento: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-      NumeroDeDocumento: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(16)]],
+      CorreoElectronico: ['', [Validators.required, this.emailValidator(), Validators.maxLength(30)]],
+    //  TipoDeDocumento: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+    //  NumeroDeDocumento: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(16)]],
       Contrasena: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(30), this.passwordValidator]],
       ConfirmarContrasena: ['', [Validators.required]],
-      Genero: ['', [Validators.minLength(8), Validators.maxLength(9)]],
-      Direccion: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(30)]],
-      Rol: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]]
+    //  Genero: ['', [Validators.minLength(8), Validators.maxLength(9)]],
+    //  Direccion: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(30)]],
+    //  Rol: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]]
     }, { validator: this.passwordMatcher });
+  }
+
+  emailValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value) {
+        return null;
+      }
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      const valid = emailRegex.test(control.value);
+      return valid ? null : { emailFormat: 'El formato del Correo Electrónico no es válido'  };
+    };
   }
 
   passwordValidator(control: FormControl): { [key: string]: any } | null {
@@ -53,10 +64,17 @@ export class GuardarCompradorComponent implements OnInit {
     return passwordValid ? null : { 'passwordStrength': true };
   }
 
-  passwordMatcher(group: FormGroup): { [key: string]: any } | null {
-    const password = group.controls['Contrasena'].value;
-    const confirmarPassword = group.controls['ConfirmarContrasena'].value;
-    return password === confirmarPassword ? null : { 'passwordMismatch': true };
+  passwordMatcher: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const group = control as FormGroup;
+    const passwordControl = group.get('Contrasena');
+    const confirmarPasswordControl = group.get('ConfirmarContrasena');
+    
+    if (passwordControl && confirmarPasswordControl) {
+      const password = passwordControl.value;
+      const confirmarPassword = confirmarPasswordControl.value;
+      return password === confirmarPassword ? null : { 'passwordMismatch': true };
+    }
+    return null;
   }
 
   get form() {
@@ -70,11 +88,19 @@ export class GuardarCompradorComponent implements OnInit {
 
   onSubmit() {    
     this.submitted = true;
-    console.log("Form value", this.myForm.value);
-    console.log("Form valid", this.myForm.valid);
-
+    
     if (this.myForm.invalid) {
-      console.log('Form is invalid:', this.myForm.errors);
+      console.log('Form errors:', this.myForm.errors);
+      Object.keys(this.myForm.controls).forEach(key => {
+        const control = this.myForm.get(key);
+        if (control) {
+          const controlErrors = control.errors;
+          if (controlErrors) {
+            Object.keys(controlErrors).forEach(keyError => {
+            console.log(`Control: ${key}, Error: ${keyError}, Value:`, controlErrors[keyError]);
+            });
+        }}
+      });
       return;
     }
 
@@ -82,7 +108,7 @@ export class GuardarCompradorComponent implements OnInit {
       .then((response: any) => {
         console.log('response', response);
       })
-      .catch((error: any) => {
+      .catch(error => {
         console.error('Error:', error);
       });
 
