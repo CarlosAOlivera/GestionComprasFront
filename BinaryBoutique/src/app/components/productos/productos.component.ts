ec45c80c979b5666489d6765ef48192e46bcccda
Carlos A Olivera <91080828+CarlosAOlivera@users.noreply.github.com>
2024-03-23 05:12:22 -0400

    se le añadio estilo y funcionalidad a las tarjetas de ProductosComponent

--- BinaryBoutique/src/app/components/productos/productos.component.ts
+++ BinaryBoutique/src/app/components/productos/productos.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../../data/product.model';
 import { SearchService } from '../../data/search.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ProductDetailDialogComponent } from '../product-detail-dialog/product-detail-dialog.component';
+
 
 
 @Component({
@@ -14,7 +17,17 @@ export class ProductosComponent implements OnInit {
 selectedProduct: any;
   modalService: any;
 
-  constructor(private searchService: SearchService) {}
+  constructor(
+    private searchService: SearchService,
+    public dialog: MatDialog
+  ) {}
+
+  openDialog(product: Product): void {
+    this.dialog.open(ProductDetailDialogComponent, {
+      width: '250px',
+      data: { product: product }
+    });
+  }
 
   ngOnInit(): void {
     this.loadProducts();
