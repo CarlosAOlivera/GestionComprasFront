8f49380d4035acf9e67feba744cc43fb02feff01
Asariel <asarielalequin@icloud.com>
2024-05-28 16:07:10 -0400

    adicion CRUD

--- BinaryBoutique/src/app/components/productos/productos.component.ts
+++ BinaryBoutique/src/app/components/productos/productos.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../../data/product.model';
+import { ProductService } from '../../services/product.service';
 import { SearchService } from '../../services/search.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ProductDetailDialogComponent } from '../product-detail-dialog/product-detail-dialog.component';
-
-
 
 @Component({
   selector: 'app-productos',
@@ -14,66 +13,55 @@ import { ProductDetailDialogComponent } from '../product-detail-dialog/product-d
 export class ProductosComponent implements OnInit {
   @Input() paraSexo: string = '';
   products: Product[] = [];
-selectedProduct: any;
-  modalService: any;
+  selectedProduct: Product | undefined;
 
   constructor(
+    private productService: ProductService,
     private searchService: SearchService,
     public dialog: MatDialog
   ) {}
+
+  ngOnInit(): void {
+    this.loadProductos();
+  }
+
+  loadProductos(): void {
+    if (this.paraSexo) {
+      this.searchService.getProducts(this.paraSexo)
+        .subscribe(
+          (products) => {
+            this.products = products;
+          },
+          (error) => {
+            console.error('Error al cargar los productos:', error);
+          }
+        );
+    } else {
+      this.productService.getAllProductos()
+        .subscribe(
+          (productos) => {
+            this.products = productos;
+          },
+          (error) => {
+            console.error('Error al cargar los productos:', error);
+          }
+        );
+    }
+  }
 
   openDialog(product: Product): void {
     this.dialog.open(ProductDetailDialogComponent, {
       width: '800px',
       maxHeight: '90vh',
-      data: { product: product }
+      data: { producto: product }
     });
   }
 
-  ngOnInit(): void {
-    this.loadMostSearchedProducts()
+  editarProducto(producto: Product): void {
+    // Implementar lógica para editar el producto
   }
 
-  /*loadProducts(): void {
-    if (this.paraSexo) {
-    this.searchService.getProducts(this.paraSexo)
-      .subscribe(
-        (products) => {
-        this.products = products;
-      },
-      (error) => {
-        console.error('Error al cargar los productos:',error)
-      }
-      );
-    }  
-  }*/
-
-  loadMostSearchedProducts(): void {
-    this.searchService.getMostSearchedProducts()
-      .subscribe(
-        (products) => {
-          this.products = products;
-        },
-        (error) => {
-          console.error('Error al cargar los productos más buscados:', error);
-        }
-      );
+  eliminarProducto(id: number): void {
+    // Implementar lógica para eliminar el producto
   }
-
-  openModal(product: Product, content: any): void {
-    this.selectedProduct = product;
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-      (result: any) => {
-      }, 
-      (reason: any) => {
-      }
-    );
-  }
-  
-
-openSidePanel() {
-throw new Error('Method not implemented.');
-}
-isSidePanelVisible: any;
-
 }
